8f4b874099ab58c29b7de038325fea552c7aaba9
junho16 <nchu17202113@163.com>
2022-03-16 16:20:55 +0800

    二叉树构建

---
+++ src/lc_2_Interview/Main113.java
@@ -0,0 +1,68 @@
+package lc_2_Interview;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 113. 路径总和 II
+ * 给你二叉树的根节点 root 和一个整数目标和 targetSum ，找出所有 从根节点到叶子节点 路径总和等于给定目标和的路径。
+ *
+ * 叶子节点 是指没有子节点的节点。
+ */
+public class Main113 {
+    List<List<Integer>> res;
+    LinkedList<Integer> path;
+    int target;
+    public List<List<Integer>> pathSum(TreeNode root, int t) {
+        target = t;
+        path = new LinkedList();
+        res = new ArrayList<>();
+        if(root == null)
+            return res;
+        dfs(root , 0);
+        return res;
+    }
+    void dfs(TreeNode root , int sum){
+        path.add(root.val);
+        if(root.left == null && root.right == null && sum + root.val == target){
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        if(root.left != null){
+            dfs(root.left , sum + root.val);
+            path.removeLast();
+        }
+        if(root.right != null){
+            dfs(root.right , sum + root.val);
+            path.removeLast();
+        }
+    }
+//    下面的代码没有优化 时间复杂度过高 因为要处理字符串
+//    List<List<Integer>> res;
+//    int target;
+//    public List<List<Integer>> pathSum(TreeNode root, int t) {
+//        target = t;
+//        res = new ArrayList<>();
+//        dfs(root , 0 , "");
+//        return res;
+//    }
+//    void dfs(TreeNode root , int sum , String path){
+//        if(root == null)
+//            return;
+//        path += (" " + root.val);
+//        if(root.left == null && root.right == null && sum + root.val == target){
+//            List<Integer> resTmp = new ArrayList<>();
+//            String[] strs = path.split(" ");
+//            for(int i = 0 ; i < strs.length ;i++){
+//                if(! strs[i].equals(""))
+//                    resTmp.add(Integer.parseInt(strs[i]));
+////                System.out.println(strs[i]);
+//            }
+//            res.add(resTmp);
+//        }
+//        dfs(root.left , sum + root.val , path);
+//        dfs(root.right , sum + root.val , path);
+//
+//    }
+}
--- src/lc_2_Interview/Main143.java
+++ src/lc_2_Interview/Main143.java
@@ -1,16 +1,71 @@
 package lc_2_Interview;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 /**
  * 143. 重排链表
  * 给定一个单链表 L 的头节点 head ，单链表 L 表示为：
- *
  * L0 → L1 → … → Ln - 1 → Ln
  * 请将其重新排列后变为：
- *
  * L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
  */
 public class Main143 {
     public void reorderList(ListNode head) {
+        if(head.next == null)
+            return;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        //找到中点
+        ListNode cur = slow;
 
+        //分成两个链表
+        ListNode p = head;
+        ListNode nullPre = p;
+        while (p != cur){
+            nullPre = p;
+            p = p.next;
+        }
+        nullPre.next = null;
+        ListNode list1 = head;
+
+        //第二条链表
+        ListNode pre = null;
+        while (cur != null){
+            ListNode tmp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        ListNode list2 = pre;
+
+        // while (list1 != null){
+        //     System.out.print(list1.val + " ");
+        //     list1 = list1.next;
+        // }
+        // System.out.println();
+        // while (list2 != null){
+        //     System.out.print(list2.val + " ");
+        //     list2 = list2.next;
+        // }
+
+        ListNode list1Tmp = null ;
+        ListNode list2Tmp = null ;
+        while (list1.next != null && list2.next != null){
+            list1Tmp = list1.next;
+            list2Tmp = list2.next;
+            list1.next = list2;
+            list2.next = list1Tmp;
+            list1 = list1Tmp;
+            list2 = list2Tmp;
+        }
+        list1.next = list2;
+        // if(list2Tmp != null)
+        //     list2.next = list2Tmp;
     }
 }
